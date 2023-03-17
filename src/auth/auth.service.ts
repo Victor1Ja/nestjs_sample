588073e741014e5e42581f61ccc023f08390b3f8
Victor Javier López Roque <victor.98.javier@gmail.com>
2023-03-17 19:03:33 -0400

    add env variables  and config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
-  login() {}
-  signup() {}
+  login(dto: AuthDto) {
+    return 'I am login';
+  }
+  signup(dto: AuthDto) {
+    const hash = argon.hash(dto.password);
+    const user = 1;
+  }
 }
