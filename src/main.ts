2c5ccae38636c30f687f3c568b46c5d5cdcd376a
Victor Javier López Roque <victor.98.javier@gmail.com>
2023-03-20 16:10:07 -0400

    Apply pipes globally

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(configService.getPort());
 }
 bootstrap();
