b9423289e6e3d83a482e27ac65817b22058a7e33
Victor Javier López Roque <victor.98.javier@gmail.com>
2023-03-20 18:45:02 -0400

    set pipe whitelist to true

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(configService.getPort());
 }
 bootstrap();
