319acb7607914279342aae99669e9164d359ddbf
Victor Javier López Roque <victor.98.javier@gmail.com>
2023-03-23 17:34:06 -0400

    Create strategy and set it to the user me endpoint

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+interface Payload {
+  sub: string;
+  email: string;
+}
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: Payload) {
+    return { sub: payload.sub, email: payload.email };
+  }
+}
